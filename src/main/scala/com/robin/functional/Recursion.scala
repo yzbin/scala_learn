93d25a729dd5c0926879c77a339a6397d3fde3bb
yzbin <jiabin126email@126.com>
2017-12-21 21:56:18 +0800

    Recursion commit

--- src/main/scala/com/robin/functional/Recursion.scala
+++ src/main/scala/com/robin/functional/Recursion.scala
@@ -8,10 +8,34 @@ package com.robin.functional
   * description:
   */
 object Recursion {
+  /**
+    * 递归函数(Recursive Function)在函数式编程中实现循环的一种技术。
+    * @param args
+    */
   def main(args: Array[String]): Unit = {
-    for(a <- 1 to 10){
-      println(facotrial(a))
+    def test1():Unit={
+      for(a <- 1 to 10){
+        println(facotrial(a))
+      }
     }
+
+    def test2():Unit={
+      /**
+        * 尾递归函数(Tail Recurrsive Function) 中所有递归形式的调用都出现在函数的末尾
+        *
+        * 当编译器检测到一个函数调用是尾递归的时候，它就覆盖当前的活动记录而不是在栈中去创建一个新的
+        */
+      @annotation.tailrec
+      def factorial(n:Int, m:Int):Int = {
+        if (n <= 0) m
+        else factorial(n-1, m * n)
+      }
+      factorial(5,1)
+
+    }
+
+//   test1()
+    test2()
   }
 
   def facotrial(n:BigInt):BigInt={
